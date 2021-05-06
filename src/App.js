657c2f4db4644674e1b627ca006186c8ce45ed97
Sawsen <sawsensassi97@gmail.com>
2021-05-05 22:51:01 -0700

    Create a form using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { Dropdown, DropdownButton} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+  <header>
+    <h1> Login in </h1>
+    <br/>
+  </header>
+  <main>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div id="dropdown">
+  <DropdownButton id="dropdown-basic-button" title="Pays">
+  <Dropdown.Item href="#/action-1">Tunisie</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">France</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Canada</Dropdown.Item>
+</DropdownButton>
+</div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</main>
+<footer>
+  Copyright @2021 by Sawsen SASSI
+</footer>
+</>
   );
 }
 
